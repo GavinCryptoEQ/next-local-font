85bf7cc82d48ee44f0ffb446d45e67e2da56636c
gavbax99 <gav.bax@gmail.com>
2022-11-20 12:06:14 -0800

    init

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+// pages/_app.js
+import localFont from '@next/font/local'
+
+// Font files can be colocated inside of `pages`
+const myFont = localFont({ src: './VisbyCF-Regular.otf' })
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <main className={myFont.className}>
+      <Component {...pageProps} />
+    </main>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+// React
+import React from "react";
+
+// CSS
+import * as Styles from "./css/Index.module.css";
+
+// ===== COMPONENT =====
+const Index = ({}) => {
+  return (
+    <div className={Styles.root}>
+      <h1>This Repo Is Cursed</h1>
+
+      <p><span>Font loader error: Cannot read property 'fs' of undefined</span> is perhaps too spooky for most. Some say fs is undefined because of goblins, and frankly I agree.</p>
+    </div>
+  );
+};
+
+export default Index;
